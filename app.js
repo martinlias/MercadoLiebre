59eed8ca77012f7f0fa5961e4ee5c3394e5c8c1b
martinlias <martinliaskowsky@gmail.com>
2021-01-09 13:53:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const path = require ("path");
+
+const app = express();
+app.listen(3000);
+
+// LOS MIDDLEWARES
+const staticFolder = path.resolve(__dirname, "./public");
+app.use(express.static(staticFolder))
+
+// LAS RUTAS
+app.get("/", (req, res) => {
+    res.sendFile (path.resolve(__dirname, "views/home.html"));
+});
