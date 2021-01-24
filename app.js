a062f0202a5b10a0de65f29a8b6caba8b149ab03
martinlias <martinliaskowsky@gmail.com>
2021-01-24 17:49:16 -0300

    Agregando nuevas rutas

--- app.js
+++ app.js
@@ -12,3 +12,7 @@ app.use(express.static(staticFolder))
 app.get("/", (req, res) => {
     res.sendFile (path.resolve(__dirname, "views/home.html"));
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile (path.resolve(__dirname, "views/register.html"));
+});
